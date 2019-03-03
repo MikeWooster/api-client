844507491da0be3f79f1adaaa7ce445707c1d73d
MikeWooster <mikewooster87@gmail.com>
2019-03-03 17:04:54 +0000

    version 1.0.0

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ import setuptools
 
 
 application_dependencies = [
-    'requests>=2.16',
+    "requests>=2.16",
 ]
 prod_dependencies = []
 test_dependencies = [
-    'pytest',
-    'pytest-env',
-    'pytest-cov',
+    "pytest",
+    "pytest-env",
+    "pytest-cov",
     "vcrpy",
 ]
 lint_dependencies = [
-    'flake8',
-    'flake8-docstrings',
-    'black',
-    'isort',
+    "flake8",
+    "flake8-docstrings",
+    "black",
+    "isort",
 ]
 docs_dependencies = []
 dev_dependencies = test_dependencies + lint_dependencies + docs_dependencies + [
-    'ipdb',
+    "ipdb",
 ]
 
 
@@ -30,32 +30,32 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name='api-client',
-    version='0.2.0',
-    description='Separate the high level client implementation from the underlying CRUD operations.',
+    name="api-client",
+    version="1.0.0",
+    description="Separate the high level client implementation from the underlying CRUD.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Mike Wooster',
-    author_email='',
-    url='https://github.com/MikeWooster/api-client',
-    python_requires='>=3.6',
+    long_description_content_type="text/markdown",
+    author="Mike Wooster",
+    author_email="",
+    url="https://github.com/MikeWooster/api-client",
+    python_requires=">=3.6",
     packages=[
-        'apiclient',
+        "apiclient",
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Intended Audience :: Developers',
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
     ],
     install_requires=application_dependencies,
     extras_require={
-        'production': prod_dependencies,
-        'test': test_dependencies,
-        'lint': lint_dependencies,
-        'docs': dev_dependencies,
-        'dev': dev_dependencies,
+        "production": prod_dependencies,
+        "test": test_dependencies,
+        "lint": lint_dependencies,
+        "docs": dev_dependencies,
+        "dev": dev_dependencies,
     },
     include_package_data=True,
     zip_safe=False,
