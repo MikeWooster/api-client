3aa22862a927fb73dd2db8dba8f46997c8b5d0ae
Rufus <73200607+dp-rufus@users.noreply.github.com>
2020-11-29 18:11:31 +0000

    Fix (un)marshal_response decorator type hints
    The decorator func returns the unevaluated inner func itself - i.e. a Callable

--- apiclient/json.py
+++ apiclient/json.py
@@ -1,4 +1,4 @@
-from typing import Optional, TypeVar
+from typing import Any, Callable, Optional, TypeVar
 
 from jsonmarshal import marshal, unmarshal
 
@@ -8,7 +8,7 @@ T = TypeVar("T")
 def unmarshal_response(schema: T, date_fmt: Optional[str] = None, datetime_fmt: Optional[str] = None):
     """Decorator to unmarshal the response json into the provided dataclass."""
 
-    def decorator(func) -> T:
+    def decorator(func) -> Callable[..., T]:
         def wrap(*args, **kwargs) -> T:
             response = func(*args, **kwargs)
             return unmarshal(response, schema, date_fmt=date_fmt, datetime_fmt=datetime_fmt)
@@ -21,7 +21,7 @@ def unmarshal_response(schema: T, date_fmt: Optional[str] = None, datetime_fmt: 
 def marshal_request(date_fmt: Optional[str] = None, datetime_fmt: Optional[str] = None):
     """Decorator to marshal the request from a dataclass into valid json."""
 
-    def decorator(func) -> T:
+    def decorator(func) -> Callable[..., Any]:
         def wrap(endpoint: str, data: T, *args, **kwargs):
             marshalled = marshal(data, date_fmt=date_fmt, datetime_fmt=datetime_fmt)
             return func(endpoint, marshalled, *args, **kwargs)
