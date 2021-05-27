55e8194344fc034c78ef6d2fab8cce62106d8673
Eduardo Jiron <emjironal@gmail.com>
2021-05-26 21:51:54 -0600

    Revert "[fix] Eliminates base_url of super and stores current base_url"
    This reverts commit 5b3ec7f3e381f2d6626acd786bc0ed96c489bf47.

--- apiclient/decorates.py
+++ apiclient/decorates.py
@@ -1,7 +1,5 @@
 import inspect
 
-BASE_URL_ATTR_NAME = "_base_url"
-
 
 def endpoint(cls_=None, base_url=None):
     """Decorator for automatically constructing urls from a base_url and defined resources."""
@@ -23,17 +21,11 @@ def _process_class(cls, base_url):
         )
     else:
         base_url = base_url.rstrip("/")
-        try:
-            super_base_url = getattr(cls, BASE_URL_ATTR_NAME)
-        except AttributeError:
-            super_base_url = ""
-        setattr(cls, BASE_URL_ATTR_NAME, base_url)
 
     for name, value in inspect.getmembers(cls):
         if name.startswith("_") or inspect.ismethod(value) or inspect.isfunction(value):
             # Ignore any private or class attributes.
             continue
-        value = str(value).replace(super_base_url, "")
         new_value = str(value).lstrip("/")
         resource = f"{base_url}/{new_value}"
         setattr(cls, name, resource)
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -27,11 +27,9 @@ class EndpointFromEnvironment:
     search = "search"
 
 
-@endpoint(base_url="https://food.com")
 class BaseEndpoint:
     get_apples = "apples"
     get_grapes = "grapes"
-
 
 @endpoint(base_url="https://fruits.com")
 class SubEndpoint(BaseEndpoint):
@@ -68,8 +66,6 @@ def test_decorated_endpoint_loaded_from_environment_variable():
 
 
 def test_decorator_inherits_attributes():
-    assert BaseEndpoint.get_apples == "https://food.com/apples"
-    assert BaseEndpoint.get_grapes == "https://food.com/grapes"
     assert SubEndpoint.get_apples == "https://fruits.com/apples"
     assert SubEndpoint.get_grapes == "https://fruits.com/grapes"
     assert SubEndpoint.get_hamburgers == "https://fruits.com/hamburgers"
